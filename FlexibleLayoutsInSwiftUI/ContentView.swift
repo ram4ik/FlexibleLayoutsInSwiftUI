5b153be81e21c7e56e65072e531bf1101494249d
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-27 22:35:50 +0300

    Initial Commit

---
+++ FlexibleLayoutsInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlexibleLayoutsInSwiftUI
+//
+//  Created by Ramill Ibragimov on 27.08.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlexibleLayoutsInSwiftUI/FlexibleLayoutsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlexibleLayoutsInSwiftUIApp.swift
+//  FlexibleLayoutsInSwiftUI
+//
+//  Created by Ramill Ibragimov on 27.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct FlexibleLayoutsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
